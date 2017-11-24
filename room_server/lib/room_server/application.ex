c1b7790ed3f50a0873ecae26bedee8cfa679f43a
aWiedebusch <awiedebusch@smu.edu>
2017-11-24 15:47:49 -0600

    added checker pngs, supervisor for individual games

--- room_server/lib/room_server/application.ex
+++ room_server/lib/room_server/application.ex
@@ -9,8 +9,8 @@ defmodule RoomServer.Application do
 
     children = [
       worker(RoomServer.RoomState, args, restart: :permanent),
-      worker(RoomServer, [], restart: :transient)
-      #supervisor(RoomServer.GameSupervisor, [])
+      worker(RoomServer, [], restart: :transient),
+      supervisor(RoomServer.GameSupervisor, [])
     ]
 
     opts = [
